bfcd3d50bd8ca392da325ce5a92df515fe3030df
germangamboa95 <germangamboa95@gmail.com>
2024-04-15 23:35:00 -0400

    testing deployment

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import express from "express";
 const app = express();
 
 app.get("/", (_, res) => {
-  return res.json({ message: "hello world" });
+  return res.json({
+    message: "hello world",
+    addition: "I was added to test deployments",
+  });
 });
 
 app.listen(process.env.PORT, (_) => {
